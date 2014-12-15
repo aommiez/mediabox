6107422871ba274cf2ecda86969f60a8f7860d77
aommiez <aommiez@gmail.com>
2014-12-15 13:35:44 +0700

    add mutiple

--- routes/index.js
+++ routes/index.js
@@ -47,71 +47,132 @@ router.delete('/:id', function(request, response){
 
 router.post("/", function(request, response){
   console.log("POST UPLOAD");
-  if ( 'images' in request.body && typeof request.body.images == "string" ) {
+  var data = request.body.images;
+  if ( typeof data == "string") {
+    if ( 'images' in request.body && typeof data == "string" ) {
 
-    // decode image
-    var decodedImage = new Buffer(request.body.images, 'base64');
-    // random file name
-    var fileName = helper.randomAsciiString(32);
+      // decode image
+      var decodedImage = new Buffer(request.body.images, 'base64');
+      // random file name
+      var fileName = helper.randomAsciiString(32);
 
-    // write file
-    fs.writeFile("public/uploads/"+fileName, decodedImage, function (err) {
-      if (err) {
-        rh.renderMsg(response,err);
-      } else {
+      // write file
+      fs.writeFile("public/uploads/"+fileName, decodedImage, function (err) {
+        if (err) {
+          rh.renderMsg(response,err);
+        } else {
 
-        // get dimesions
-        var dimensions = sizeOf("public/uploads/"+fileName);
-        // get type
-        var imgType = imageType(readChunk.sync("public/uploads/"+fileName, 0, 12));
-        var extAllow = ['jpg','png','jpge','JPG'];
+          // get dimesions
+          var dimensions = sizeOf("public/uploads/"+fileName);
+          // get type
+          var imgType = imageType(readChunk.sync("public/uploads/"+fileName, 0, 12));
+          var extAllow = ['jpg','png','jpge','JPG'];
 
-        // check file ext
-        if (extAllow.indexOf(imgType) == -1 ) {
-          rh.renderMsg(response, "ext not allow");
-          return;
-        }
-
-        // rename file
-        fs.rename("public/uploads/"+fileName, "public/uploads/"+fileName+"."+imgType, function(err) {
-          if ( err ) {
-            rh.renderMsg(response,err);
-          } else {
-            fs.chmodSync("public/uploads/"+fileName+"."+imgType, 0777);
-            var images = new Images({
-              base64data : request.body.images ,
-              name : fileName+"."+imgType ,
-              image_info: {
-                dimensions: {
-                  width : dimensions.width ,
-                  height : dimensions.height
-                },
-                type: imgType
-              } ,
-              create_date : helper.getTimeStamp()
-            });
-
-            images.save(function (err,resSave) {
-              if (err) {
-                rh.renderMsg(response,err);
-              } else {
-                var r = resSave.toObject();
-                delete r.name;
-                response.json(r);
-              }
-            });
+          // check file ext
+          if (extAllow.indexOf(imgType) == -1 ) {
+            rh.renderMsg(response, "ext not allow");
+            return;
           }
-        });
+
+          // rename file
+          fs.rename("public/uploads/"+fileName, "public/uploads/"+fileName+"."+imgType, function(err) {
+            if ( err ) {
+              rh.renderMsg(response,err);
+            } else {
+              fs.chmodSync("public/uploads/"+fileName+"."+imgType, 0777);
+              var images = new Images({
+                base64data : request.body.images ,
+                name : fileName+"."+imgType ,
+                image_info: {
+                  dimensions: {
+                    width : dimensions.width ,
+                    height : dimensions.height
+                  },
+                  type: imgType
+                } ,
+                create_date : helper.getTimeStamp()
+              });
+
+              images.save(function (err,resSave) {
+                if (err) {
+                  rh.renderMsg(response,err);
+                } else {
+                  var r = resSave.toObject();
+                  delete r.name;
+                  response.json(r);
+                }
+              });
+            }
+          });
+        }
+      });
+      // end write file
+    } else if ( 'images' in request.body ) {
+
+    } else {
+      // response upload error
+      rh.renderMsg(response,"require images base64");
+    }
+  } else if (typeof data == "object")  {
+    var resArr = [];
+    //console.log(data);
+    for (var i in data) {
+
+      // decode image
+      var decodedImage = new Buffer(data[i], 'base64');
+      // random file name
+      var fileName = helper.randomAsciiString(32);
+
+
+      fs.writeFileSync("public/uploads/"+fileName, decodedImage);
+      // end write file
+
+      // get dimesions
+      var dimensions = sizeOf("public/uploads/"+fileName);
+      // get type
+      var imgType = imageType(readChunk.sync("public/uploads/"+fileName, 0, 12));
+      var extAllow = ['jpg','png','jpge','JPG'];
+
+      // check file ext
+      if (extAllow.indexOf(imgType) == -1 ) {
+        rh.renderMsg(response, "ext not allow");
+        return;
       }
-    });
-    // end write file
-  } else if ( 'images' in request.body ) {
 
-  } else {
-    // response upload error
-    rh.renderMsg(response,"require images base64");
+      fs.renameSync("public/uploads/"+fileName, "public/uploads/"+fileName+"."+imgType);
+      fs.chmodSync("public/uploads/"+fileName+"."+imgType, 0777);
+
+      var images = new Images({
+        base64data : data[i] ,
+        name : fileName+"."+imgType ,
+        image_info: {
+          dimensions: {
+            width : dimensions.width ,
+            height : dimensions.height
+          },
+          type: imgType
+        } ,
+        create_date : helper.getTimeStamp()
+      });
+      //console.log(images);
+
+      images.save(function (err,resSave) {
+        if (err) {
+          rh.renderMsg(response,err);
+        } else {
+          var r = resSave.toObject();
+          delete r.name;
+          //response.json(r);
+          resArr.push(r);
+          //console.log(resSave);
+          if (resArr.length == data.length ) {
+            response.send(resArr);
+          }
+        }
+      });
+    }
   }
-});
 
+});
 
 module.exports = router;
