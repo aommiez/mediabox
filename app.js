a5a1db02a1e4000b8ffb544a1fab6fa9ca9a7df3
aommiez <aommiez@gmail.com>
2014-12-16 12:53:40 +0700

    update get images

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use('/', routes);
 
 
